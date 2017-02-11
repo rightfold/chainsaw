7ef1d86cd2587ad3920a9ae9b68662a064563219
rightfold <rightfold@gmail.com>
2017-02-11 23:55:48 +0100

    Append records to the logs

--- src/bin/chainsawd.rs
+++ src/bin/chainsawd.rs
@@ -2,11 +2,14 @@ extern crate chainsaw;
 
 use chainsaw::config;
 use chainsaw::config::Config;
+use chainsaw::log;
 use chainsaw::zmq;
 use std::env;
 use std::io;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use std::thread::{JoinHandle, spawn};
+use std::time::SystemTime;
 
 /*----------------------------------------------------------------------------*/
 
@@ -43,10 +46,12 @@ fn safe_main() -> Result<(), Error> {
     let config_path = try!(env::args().nth(1).ok_or(Error::MissingConfigPath));
     let config = try!(Config::new_from_file(config_path));
 
+    let store = Arc::new(config.store);
+
     let zmq = Arc::new(try!(zmq::Context::new()));
     let mut pub_ = try!(make_pub(&zmq));
 
-    for logger in start_loggers(&zmq, config.logs.iter().cloned()) {
+    for logger in start_loggers(&store, &zmq, config.logs.iter().cloned()) {
         logger.join().unwrap().unwrap();
     }
 
@@ -59,24 +64,29 @@ fn make_pub(zmq: &zmq::Context) -> io::Result<zmq::Socket> {
     Ok(pub_)
 }
 
-fn start_loggers<I>(zmq: &Arc<zmq::Context>, logs: I)
+fn start_loggers<I>(store: &Arc<PathBuf>, zmq: &Arc<zmq::Context>, logs: I)
     -> Vec<JoinHandle<io::Result<()>>>
     where I: Iterator<Item=String> {
     logs
     .map(|log| {
+        let store = store.clone();
         let zmq = zmq.clone();
-        spawn(move || { run_logger(&zmq, &log) })
+        spawn(move || { run_logger(&store, &zmq, &log) })
     })
     .collect()
 }
 
-fn run_logger(zmq: &zmq::Context, log: &str) -> io::Result<()> {
+fn run_logger(store: &Path, zmq: &zmq::Context, log: &str) -> io::Result<()> {
     let mut sub = try!(zmq::Socket::new(zmq, zmq::SocketType::SUB));
     try!(sub.connect(&mut INPROC_ADDRESS.to_vec()));
     try!(sub.subscribe(log.as_bytes()));
 
+    let clock = || SystemTime::now();
+    let mut appender = try!(log::open_for_append(clock, store, log));
+
     let mut message = zmq::Message::new();
     loop {
         try!(sub.receive(&mut message));
+        try!(appender.append(message.data()));
     }
 }
